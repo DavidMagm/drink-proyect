4bd1547bdc6113a205049ff348f5a74e0e0040a4
David Torres <david.martinezezguerra@gmail.com>
2024-09-27 22:33:38 -0500

    primero estraer los valores del rate segundo crear la tabla del rate chart

--- src/Componets/Tables/index.js
+++ src/Componets/Tables/index.js
@@ -5,21 +5,20 @@ import './index.css'
 
 function Tables() {
 
-    const {items} = useContext(MoneyPageContext)
+    const {items, setItemsValue} = useContext(MoneyPageContext)
     
     
     let nameAndRate = Object.entries(items)
     //let array = Object.values(items)
     
-     console.log(nameAndRate)
     
     return(
         <>
            {
             nameAndRate.map((name,index) => (
                     <div key={index} className="table-rate-name">
-                        <NavLink to={'rate'}>{name[0]}</NavLink>
-                        <NavLink to={'rate'}><span className="table-rate-number">{name[1]}</span></NavLink>
+                        <NavLink to={'rate'}><div onClick={() => setItemsValue(name)}>{name[0]}</div></NavLink>
+                        <span className="table-rate-number">{name[1]}</span>
                     </div>
                 ))
            }
--- src/Context/index.js
+++ src/Context/index.js
@@ -6,7 +6,12 @@ export function MoneyPageProvider({children}) {
 
     const url = 'https://api.frankfurter.app';
 
-    const [items,setItems] = useState([])
+    const [items,setItems] = useState([]);
+    const [itemsValue, setItemsValue] = useState([]);
+    const [rateDay, setRateDay] = useState([]);
+    const [rateWeek, setRateWeek] = useState([]);
+    const [rateYears, setRateYears] = useState([]);
+    const [dateToday, setDateToday] = useState("");
 
     useEffect(() => {
         fetch(`${url}/latest`)
@@ -14,8 +19,18 @@ export function MoneyPageProvider({children}) {
             .then(data => setItems(data.rates))
     },[])
 
+
+    useEffect(() => {
+        fetch(`https://api.frankfurter.app/latest`)
+            .then(res => res.json())
+            .then(data => setDateToday(data.date))
+    }, [])
+    
+
+    
+
     return(
-        <MoneyPageContext.Provider value={{items}}>
+        <MoneyPageContext.Provider value={{items,itemsValue,setItemsValue,dateToday,rateWeek,setRateWeek, rateYears, setRateYears,rateDay, setRateDay}}>
             {children}
         </MoneyPageContext.Provider>
     )
