c794a378a13fcbc86b366e0574208903671a4302
David Torres <david.martinezezguerra@gmail.com>
2024-12-17 20:36:12 -0500

    query dinamyc complete name currency

--- src/Componets/InfoRateTable/index.js
+++ src/Componets/InfoRateTable/index.js
@@ -1,14 +1,23 @@
-import { useContext } from 'react';
-//import { useEffect } from 'react';
+import { useContext, useEffect } from 'react';
 import { MoneyPageContext } from '../../Context';
-import './index.css';
 import { TableRate } from '../TableRate';
 import { NewsFinalSection } from '../NewsFinalSection';
+import './index.css';
 
 
 
 function InfoRateTable() {
-    const {infoMoneyRate} = useContext(MoneyPageContext)
+
+    const {infoMoneyRate,setTopNewsBusiness} = useContext(MoneyPageContext)
+
+    let queryCurrencyName = infoMoneyRate.completeName.toLowerCase()
+    let regexQueryCurrency = queryCurrencyName.replace(/\s+/g, '-')
+
+    useEffect(() => {
+        fetch(`https://newsapi.org/v2/everything?q=${regexQueryCurrency}&pageSize=10&apiKey=6e9123d54a31446e82cdd97208d8c7fb`)
+        .then(response => response.json())
+        .then(data => setTopNewsBusiness(data.articles))
+    },[regexQueryCurrency, setTopNewsBusiness])
     
     return(
         <section className="section-info-rate">
